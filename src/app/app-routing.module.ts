4492942f03ff424319ab54354c0baea76f8eb7cf
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-29 21:19:08 +0300

    Moved components to components folder Added Github Connect main routes Add Github Connect router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GConnectHomeComponent } from './components/g-connect-home/g-connect-home.component';
+import { GConnectSearchUserComponent } from './components/g-connect-search-user/g-connect-search-user.component';
+import { GConnectSearchRepoComponent } from './components/g-connect-search-repo/g-connect-search-repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:GConnectHomeComponent},
+  {path:'search-user',component:GConnectSearchUserComponent},
+  {path:'search-repo',component:GConnectSearchRepoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GConnectHomeComponent } from './g-connect-home/g-connect-home.component';
-import { GConnectNavbarComponent } from './g-connect-navbar/g-connect-navbar.component';
-import { GConnectSearchUserComponent } from './g-connect-search-user/g-connect-search-user.component';
-import { GConnectSearchRepoComponent } from './g-connect-search-repo/g-connect-search-repo.component';
+import { GConnectHomeComponent } from './components/g-connect-home/g-connect-home.component';
+import { GConnectNavbarComponent } from './components/g-connect-navbar/g-connect-navbar.component';
+import { GConnectSearchUserComponent } from './components/g-connect-search-user/g-connect-search-user.component';
+import { GConnectSearchRepoComponent } from './components/g-connect-search-repo/g-connect-search-repo.component';
 
 @NgModule({
   declarations: [
