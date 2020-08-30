d1ab159452cfa28b8d663245635a356996fb719f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-30 23:24:09 +0300

    Added github access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  github_token:'7c618ab79cd369eeb76c8f63a74acf599e32f631'
 };
 
 /*
