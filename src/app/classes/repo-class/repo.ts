86e2f6de0b1a9c1d5745b78c58e70b27157106af
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-30 23:10:49 +0300

    Created github connect repo class

---
+++ src/app/classes/repo-class/repo.ts
@@ -0,0 +1,5 @@
+export class Repo {
+  constructor(public id:number,public name:any,public owner:any,public repoUrl:any,public description:any,public createdAT:Date,public watchers:number,public language:any,public forks:number){
+
+  }
+}
--- src/app/classes/user-class/user.ts
+++ src/app/classes/user-class/user.ts
@@ -1,5 +1,5 @@
 export class User {
-  constructor(public id:number,public name:any,public username:any,public company:any,public blog:any,public location:any,public bio:any,public repos:number,public followers:number,public following:number,public createdAt:Date){
-    
+  constructor(public id:number,public name:any,public userUrl:any,public userImgUrl:any,public username:any,public company:any,public blog:any,public location:any,public bio:any,public repos:number,public followers:number,public following:number,public createdAt:Date){
+
   }
 }
