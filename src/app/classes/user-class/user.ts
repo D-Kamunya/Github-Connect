5379d49f99fcd65e432960ce1acb95f80fbd322d
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-31 11:27:29 +0300

    Editted user class properties

--- src/app/classes/user-class/user.ts
+++ src/app/classes/user-class/user.ts
@@ -1,5 +1,5 @@
 export class User {
-  constructor(public id:number,public name:any,public userUrl:any,public userImgUrl:any,public username:any,public company:any,public blog:any,public location:any,public bio:any,public repos:number,public followers:number,public following:number,public createdAt:Date){
+  constructor(public id:number,public name:string,public html_url:string,public avatar_url:string,public login:string,public company:string,public blog:string,public location:string,public bio:string,public public_repos:number,public followers:number,public following:number,public created_at:Date){
 
   }
 }
