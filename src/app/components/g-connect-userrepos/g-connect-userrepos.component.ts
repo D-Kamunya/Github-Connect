dfb32bd9736a33af44fadeaa38d1a064229e5562
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-01 19:15:04 +0300

    Used sevice to getuserrepo and display using ngfor

--- src/app/components/g-connect-userrepos/g-connect-userrepos.component.ts
+++ src/app/components/g-connect-userrepos/g-connect-userrepos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  ActivatedRoute, ParamMap } from '@angular/router';
+import {GithubApiService} from '../../services/github-api/github-api.service'
+import {Repo} from '../../classes/repo-class/repo'
 
 @Component({
   selector: 'app-g-connect-userrepos',
@@ -9,10 +11,20 @@ import {  ActivatedRoute, ParamMap } from '@angular/router';
 export class GConnectUserreposComponent implements OnInit {
 
   username:string
-  constructor(private route:ActivatedRoute) { }
+  userrepos:Repo
+
+  constructor(private route:ActivatedRoute,private apiService:GithubApiService) { }
 
   ngOnInit(): void {
-    this.username = this.route.snapshot.paramMap.get('username');
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.apiService.getUserRepos(this.username).then((success)=>{
+      this.userrepos = this.apiService.userrepos;
+      console.log(this.userrepos);
+      
+    },
+    (error)=>{
+      console.log(error)
+    })
   }
 
 }
